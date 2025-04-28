c71952f3f669ce287b0dd66f844e1492bbd766ee
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-28 19:23:05 -0300

    formatting the currency

--- script.js
+++ script.js
@@ -4,5 +4,19 @@ const amount = document.getElementById("amount")
 // Validation Only Numbers
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
-  amount.value = value
+
+  // transform the value in cents (Ex: 150/100 = 1.5 -> R$ 1,50)
+  value = Number(value) / 100
+
+  // Updating the input value, using the function
+  amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+
+  return value
 }
