968985dce0b16342811db1975e6c8c490132f191
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-28 19:12:12 -0300

    validation: Only Numbers

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Select the form elements
 const amount = document.getElementById("amount")
 
+// Validation Only Numbers
 amount.oninput = () => {
-  console.log("Test New Content")
+  let value = amount.value.replace(/\D/g, "")
+  amount.value = value
 }
